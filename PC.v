5bedf54c1e5a647130503e1f199949cf043d3824
Weiyuan Du <145103663+SinYita@users.noreply.github.com>
2025-12-01 13:09:25 +0000

    update more

--- PC.v
+++ PC.v
@@ -1,12 +1,12 @@
-module PC(clk,rst, NPC,PC);
+module PC(clk,rst,PC,NPC);
     input clk;
     input rst;
     input [31:0] NPC;
     output reg [31:0] PC;
 
     always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            PC <= 32'h0000_3000;
+        if (~rst) begin
+            PC <= {32{1'b0}};
         end
         else begin
             PC <= NPC;
